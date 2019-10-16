6e337c37107dfb467ab64e7f75669239c529d82e
Adam <s15814@pjwstk.edu.pl>
2019-10-16 19:49:29 +0200

    Implemented the builder class

--- src/main/java/model/TvShowBuilder.java
+++ src/main/java/model/TvShowBuilder.java
@@ -8,19 +8,36 @@ public class TvShowBuilder {
     private String platform;
 
     public TvShowBuilder byId (int id) {
+        this.id = id;
+        return this;
     }
 
     public TvShowBuilder byTitle (String title) {
+        this.title = title;
+        return this;
     }
 
     public TvShowBuilder byNumberOfSeasons (int numberOfSeasons) {
+        this.numberOfSeasons = numberOfSeasons;
+        return this;
     }
 
     public TvShowBuilder byDirectorsName (String directorsName) {
+        this.directorsName = directorsName;
+        return this;
     }
     public TvShowBuilder byPlatform (String platform) {
+        this.platform = platform;
+        return this;
     }
 
     public TvShow build() {
+        TvShow tvShow = new TvShow();
+        tvShow.setId(id);
+        tvShow.setTitle(title);
+        tvShow.setNumberOfSeasons(numberOfSeasons);
+        tvShow.setDirectorsName(directorsName);
+        tvShow.setPlatform(platform);
+        return tvShow;
     }
 }
